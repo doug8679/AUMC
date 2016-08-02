30ea0a84fa5097a7788904588ce403536702bd1c
Doug Braxton <doug@deltasgimacompany.com>
2016-08-02 15:02:09 -0400

    Started a repository for church-related code work.

---
+++ AUMC Utilities/DockAPI_Routines/DockRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockAPI_Routines
+{
+    public class DockRequest {
+        private HttpClient client;
+
+        public DockRequest() {
+            client = new HttpClient();
+            var requestContent = new FormUrlEncodedContent(new[] {new KeyValuePair<string, string>(), });
+        }
+    }
+}
